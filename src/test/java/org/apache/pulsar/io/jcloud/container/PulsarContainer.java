036e04c51fd381598a63e4c4bec8728b43fff3d3
Rui Fu <freelz@gmail.com>
2021-07-26 13:41:50 +0800

    remove instance dep from tests

---
+++ src/test/java/org/apache/pulsar/io/jcloud/container/PulsarContainer.java
@@ -0,0 +1,93 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pulsar.io.jcloud.container;
+
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.containers.wait.strategy.WaitAllStrategy;
+import org.testcontainers.utility.DockerImageName;
+
+/**
+ * PulsarContainer uses streamnative images.
+ */
+public class PulsarContainer extends GenericContainer<PulsarContainer> {
+
+    public static final int BROKER_PORT = 6650;
+    public static final int BROKER_HTTP_PORT = 8080;
+    public static final String METRICS_ENDPOINT = "/metrics";
+
+    private static final DockerImageName DEFAULT_IMAGE_NAME = DockerImageName.parse("streamnative/pulsar");
+    @Deprecated
+    private static final String DEFAULT_TAG = "2.8.0.7";
+
+    private boolean functionsWorkerEnabled = false;
+
+    /**
+     * @deprecated use {@link PulsarContainer(DockerImageName)} instead
+     */
+    @Deprecated
+    public PulsarContainer() {
+        this(DEFAULT_IMAGE_NAME.withTag(DEFAULT_TAG));
+    }
+
+    /**
+     * @deprecated use {@link PulsarContainer(DockerImageName)} instead
+     */
+    @Deprecated
+    public PulsarContainer(String pulsarVersion) {
+        this(DEFAULT_IMAGE_NAME.withTag(pulsarVersion));
+    }
+
+    public PulsarContainer(final DockerImageName dockerImageName) {
+        super(dockerImageName);
+
+        dockerImageName.assertCompatibleWith(DockerImageName.parse("streamnative/pulsar"));
+
+        withExposedPorts(BROKER_PORT, BROKER_HTTP_PORT);
+        withCommand("/pulsar/bin/pulsar", "standalone", "--no-functions-worker", "-nss");
+        waitingFor(Wait.forHttp(METRICS_ENDPOINT).forStatusCode(200).forPort(BROKER_HTTP_PORT));
+    }
+
+    @Override
+    protected void configure() {
+        super.configure();
+
+        if (functionsWorkerEnabled) {
+            withCommand("/pulsar/bin/pulsar", "standalone");
+            waitingFor(
+                    new WaitAllStrategy()
+                            .withStrategy(waitStrategy)
+                            .withStrategy(Wait.forLogMessage(".*Function worker service started.*", 1))
+            );
+        }
+    }
+
+    public PulsarContainer withFunctionsWorker() {
+        functionsWorkerEnabled = true;
+        return this;
+    }
+
+    public String getPulsarBrokerUrl() {
+        return String.format("pulsar://%s:%s", getHost(), getMappedPort(BROKER_PORT));
+    }
+
+    public String getHttpServiceUrl() {
+        return String.format("http://%s:%s", getHost(), getMappedPort(BROKER_HTTP_PORT));
+    }
+}
