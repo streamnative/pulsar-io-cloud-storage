bf165602950e1d154826327ac538fc05d3c732dd
Baodi Shi <baodi@apache.org>
2023-10-24 19:51:36 +0800

    Optimize Azure blob storage connector config validation logic

--- src/main/java/org/apache/pulsar/io/jcloud/writer/AzureBlobWriter.java
+++ src/main/java/org/apache/pulsar/io/jcloud/writer/AzureBlobWriter.java
@@ -18,7 +18,6 @@
  */
 package org.apache.pulsar.io.jcloud.writer;
 
-import static org.apache.commons.lang3.StringUtils.isEmpty;
 import static org.apache.commons.lang3.StringUtils.isNotEmpty;
 import com.azure.core.util.BinaryData;
 import com.azure.storage.blob.BlobClient;
@@ -36,21 +35,22 @@ import org.apache.pulsar.io.jcloud.sink.CloudStorageSinkConfig;
 @Slf4j
 public class AzureBlobWriter implements BlobWriter {
 
-    private final BlobContainerClient containerClient;
+    public final BlobContainerClient containerClient;
 
     public AzureBlobWriter(CloudStorageSinkConfig sinkConfig) {
         BlobContainerClientBuilder containerClientBuilder = new BlobContainerClientBuilder();
-        containerClientBuilder.endpoint(sinkConfig.getEndpoint());
         containerClientBuilder.containerName(sinkConfig.getBucket());
 
-        if (isNotEmpty(sinkConfig.getAzureStorageAccountSASToken())) {
+        if (isNotEmpty(sinkConfig.getAzureStorageAccountConnectionString())) {
+            containerClientBuilder.connectionString(sinkConfig.getAzureStorageAccountConnectionString());
+        } else if (isNotEmpty(sinkConfig.getAzureStorageAccountSASToken())) {
+            containerClientBuilder.endpoint(sinkConfig.getEndpoint());
             containerClientBuilder.sasToken(sinkConfig.getAzureStorageAccountSASToken());
-        } else if (!isEmptyAccountNameAccountKey(sinkConfig)) {
+        } else if (isNotEmpty(sinkConfig.getAzureStorageAccountName())
+                              && isNotEmpty(sinkConfig.getAzureStorageAccountKey())) {
+            containerClientBuilder.endpoint(sinkConfig.getEndpoint());
             containerClientBuilder.credential(new StorageSharedKeyCredential(sinkConfig.getAzureStorageAccountName(),
                     sinkConfig.getAzureStorageAccountKey()));
-        } else if (isNotEmpty(sinkConfig.getAzureStorageAccountConnectionString())) {
-            containerClientBuilder.credential(StorageSharedKeyCredential.fromConnectionString(
-                    sinkConfig.getAzureStorageAccountConnectionString()));
         } else {
             throw new IllegalArgumentException("Either azureStorageAccountSASToken or "
                     + "azureStorageAccountName and azureStorageAccountKey or "
@@ -66,13 +66,7 @@ public class AzureBlobWriter implements BlobWriter {
         blobClient.upload(BinaryData.fromBytes(payload.array()));
     }
 
-    private static boolean isEmptyAccountNameAccountKey(CloudStorageSinkConfig sinkConfig) {
-        return (isEmpty(sinkConfig.getAzureStorageAccountName()) || isEmpty(sinkConfig.getAzureStorageAccountKey()));
-    }
-
-
     @Override
     public void close() throws IOException {
-
     }
 }
