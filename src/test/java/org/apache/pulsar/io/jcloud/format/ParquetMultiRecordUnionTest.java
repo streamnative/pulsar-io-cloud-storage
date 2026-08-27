5834e181cfe3c96c9082d6629da78d0c6de786da
Rui Fu <rfu@apache.org>
2026-08-28 00:18:00 +0800

    Fix CI checks

--- src/test/java/org/apache/pulsar/io/jcloud/format/ParquetMultiRecordUnionTest.java
+++ src/test/java/org/apache/pulsar/io/jcloud/format/ParquetMultiRecordUnionTest.java
@@ -22,7 +22,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
 import java.io.ByteArrayOutputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Collections;
